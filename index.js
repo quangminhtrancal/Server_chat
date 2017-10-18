d933dbc0ab7295e8e9e21b764d60f9729c2f7f0c
quangminhtrancal <leap.ahead.tran@gmail.com>
2017-10-18 11:17:01 -0600

    Add in receiving and emitting message

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(2016, function(){
-  console.log("server port 2016");
+server.listen(2017, function(){
+  console.log("server port 2017");
   io.on('connection', function (socket) {
     console.log("There is a client's connection");
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+    socket.on('Name',function(data){
+      console.log(data)
+      data = "Received: "+data
+      socket.emit('Data',data)
+    })
+    });
 })
