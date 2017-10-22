8a5405623afd31002412ab5a3bd370f387ded36d
quangminhtrancal <leap.ahead.tran@gmail.com>
2017-10-22 00:28:33 -0600

    Update the return message during connection

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ server.listen(2017, function(){
   console.log("server port 2017");
   io.on('connection', function (socket) {
     console.log("There is a client's connection");
+    socket.emit('Data',"Server received connection")
     socket.on('Name',function(data){
       console.log(data)
       data = "Received: "+data
       socket.emit('Data',data)
+      console.log("socket id: "+socket.id)
     })
     });
 })
